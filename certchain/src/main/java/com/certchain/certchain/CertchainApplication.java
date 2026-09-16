46631a07b055305990392cb2348bad4adb299cef
Yash Ambarraj Bagal <yashbagal623@gmail.com>
2026-09-16 18:01:42 +0530

    Initial Commit: Backend Scaffholding

---
+++ certchain/src/main/java/com/certchain/certchain/CertchainApplication.java
@@ -0,0 +1,13 @@
+package com.certchain.certchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertchainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertchainApplication.class, args);
+    }
+
+}
---
+++ certchain/src/test/java/com/certchain/certchain/CertchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certchain.certchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertchainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
